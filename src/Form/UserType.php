671ad5c055457c557f127784d1589f49167f1a3a
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-06-12 14:04:02 +0200

    repirse de du projet pour la seconde session, contraint ajouté pour qu'on ne puisse s'inscrire que avec une adresse email

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -8,16 +8,18 @@ use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Choice;
+use Symfony\Component\Validator\Constraints\EmailValidator;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class)
             // ->add('roles')
             ->add('password', PasswordType::class)
             ->add('firstname')
