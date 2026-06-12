f43ade17a29758c75488d23de96ca9a26cffd994
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-06-12 14:19:50 +0200

    seulement l'admin peut voir la liste des utilisateur inscrit sur le site

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,10 +11,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/user')]
 final class UserController extends AbstractController
 {
+    #[IsGranted('ROLE_ADMIN')]
     #[Route(name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
