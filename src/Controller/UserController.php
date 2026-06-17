f1fe416b47d832ae0f532d19079e9c9bdd6b2e48
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-06-17 17:42:48 +0200

    c

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -52,7 +52,7 @@ final class UserController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_show_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/new.html.twig', [
