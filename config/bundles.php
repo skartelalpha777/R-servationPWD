05906354d1af68ddf5ab9d6da11aa2f0221b4955
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-06-19 13:43:03 +0200

    ajout de dependance pour easyAdmin

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
