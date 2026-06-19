733b02a7e3f26b14688c4cbe4ebd1dc1b8ff189c
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2026-06-19 14:08:40 +0200

    template modifié

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -28,7 +28,7 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        return $this->render('admin/dashboard/admin.html.twig');
+        return $this->render('admin/dashboard.admin.html.twig');
     }
 
     public function configureDashboard(): Dashboard
