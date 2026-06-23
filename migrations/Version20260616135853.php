3c46f1aa6d1cde771a5b1be5fe7a6069941e4d4d
Mamadou A DIALLO <3010madiallo@student.epfc.eu>
2026-06-23 14:40:06 +0200

    correction du fichier de migration

--- migrations/Version20260616135853.php
+++ migrations/Version20260616135853.php
@@ -20,7 +20,6 @@ final class Version20260616135853 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE tags');
         $this->addSql('ALTER TABLE location CHANGE slug slug VARCHAR(60) DEFAULT NULL, CHANGE designation designation VARCHAR(60) NOT NULL, CHANGE phone phone VARCHAR(30) DEFAULT NULL');
         $this->addSql('ALTER TABLE price CHANGE price price NUMERIC(10, 2) NOT NULL');
     }
@@ -28,7 +27,6 @@ final class Version20260616135853 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE tags (id INT AUTO_INCREMENT NOT NULL, tag VARCHAR(30) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, the_show_id INT DEFAULT NULL)');
         $this->addSql('ALTER TABLE location CHANGE slug slug VARCHAR(255) DEFAULT NULL, CHANGE designation designation VARCHAR(255) NOT NULL, CHANGE phone phone VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE price CHANGE price price NUMERIC(10, 0) NOT NULL');
     }
